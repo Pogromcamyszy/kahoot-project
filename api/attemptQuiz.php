d493af63fa29417ecabac624720e55b5f30b7056
Pogromcamyszy <mateusz.para@microsoft.wsei.edu.pl>
2025-04-27 02:51:41 +0200

    make attempt api builded to step where it checks if user input make sens 50% builded

--- api/attemptQuiz.php
+++ api/attemptQuiz.php
@@ -4,5 +4,38 @@
 
  $data = json_decode(file_get_contents('php://input'), true);
  $userId = (int) $user['data']->user_id;
- echo json_encode($userId);
+ $quizId = (int) $data['quiz_id'];
+ $answers = $data['answers'];
+
+ // take the quiz id and grab questions id from database to check if user input is valid
+ $questionsToValidate = $conn->prepare("SELECT * FROM questions WHERE quiz_id = $quizId");
+ $questionsToValidate->execute();
+ $questions = $questionsToValidate->fetchAll(PDO::FETCH_ASSOC);
+
+
+ // compare user answers with the questions if ids for quiz are correct
+ $questionsToCheck = [];
+ $answersToCheck = [];
+ foreach ($questions as $item){
+    $questionsToCheck[] = $item['question_id'];
+ }
+ foreach ($answers as $item){
+    $answersToCheck[] = $item['question_id'];
+ }
+ $areQuestionsValid = True;
+ for($i=0;$i<count($questionsToCheck);$i++){
+    if($questionsToCheck[$i] != $answersToCheck[$i]){
+        $areQuestionsValid = False;
+        break;
+    }
+ }
+
+ if($areQuestionsValid){
+    echo "correct";
+ }
+
+ else{
+    echo "incorrect";
+ }
+
  ?>
